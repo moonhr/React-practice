f3efbde86ae7638e2dac093e01d8bf9536119294
moonhr <moonhr153@gmail.com>
2024-07-11 12:04:24 +0900

    정적 텍스트파일을 읽기위한 속성 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/app.tsx",
+  entry: "./src/index.tsx",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "app.bundle.js",
+    filename: "index.bundle.js",
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader","css-loader"],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.js|jsx|ts|tsx$/,
@@ -21,6 +21,10 @@ module.exports = {
         test: /\.(ts|tsx)$/,
         use: "ts-loader",
         exclude: /node_modules/,
+      },
+      {
+        test: /\.(txt|md|csv)$/i, // 텍스트 파일 확장자 추가
+        use: "raw-loader", // raw-loader를 사용하여 파일을 문자열로 가져옴
       },
     ],
   },
